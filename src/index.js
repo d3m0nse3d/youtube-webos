79c5010f6ed88b07acd196c4c70337aae9b1c98c
d3m0nse3d <82076346+d3m0nse3d@users.noreply.github.com>
2026-08-13 17:37:42 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { extractLaunchParams, handleLaunch } from './utils';
 
 function main() {
-  handleLaunch(extractLaunchParams());
+    handleLaunch(extractLaunchParams());
+
+    // Add Listener to button "1" on Remote to Reload App
+    window.addEventListener('keydown', (event) => {
+        if (event.keyCode === 49 || event.key === '1') {
+            window.location.reload();
+            event.preventDefault();
+        }
+    });
 }
 
 main();
